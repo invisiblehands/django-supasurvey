82b746c21514dabcd455af5ae5e068cd9b0b8437
Cody Redmond <codydjango@gmail.com>
2015-03-12 20:24:40 -0700

    remove cruft

--- supasurvey/utils.py
+++ supasurvey/utils.py
@@ -6,29 +6,6 @@ from django.utils.html import conditional_escape
 from django.conf import settings
 
 
-
-def flatatt(attrs):
-    """
-    Convert a dictionary of attributes to a single string.
-    The returned string will contain a leading space followed by key="value",
-    XML-style pairs.  It is assumed that the keys do not need
-    to be XML-escaped.
-    If the passed dictionary is empty, then return an empty string.
-    If the value passed is None writes only the attribute (eg. required)
-    """
-    ret_arr = []
-    for k, v in attrs.items():
-
-        if v is None:
-            ret_arr.append(u' %s' % k)
-        else:
-            ret_arr.append(u' %s="%s"' % (k, conditional_escape(v)))
-    return u''.join(ret_arr)
-
-
-
-
-
 # # http://stackoverflow.com/questions/1846135/python-csv-library-with-unicode-utf-8-support-that-just-works
 class UnicodeCSVReader(object):
     def __init__(self, f, encoding="utf-8", **kwargs):
